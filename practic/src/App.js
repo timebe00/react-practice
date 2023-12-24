92daf78b6b037929a1f384fee30ff92a97a115c4
timebe00 <an0160@naver.com>
2023-12-24 16:02:14 +0900

    props 사용 연습

--- practic/src/App.js
+++ practic/src/App.js
@@ -1,41 +1,51 @@
 import './App.css';
 
 //  컴포넌트 생성
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1><a href="/">Web</a></h1>
+      <h1><a href="/">{props.title}</a></h1>
     </header>
   )
 }
 
-function MenuList() {
+function Nav(props) {
+  let lis = [];
+  let topics = props.topics;
+
+  topics.map((element)=> {
+    lis.push(<li key={element.id}> <a href={"/read/" +element.id}>{element.title}</a> </li>)
+  })
+
   return (
     <nav>
       <ol>
-        <li> <a href="/read/1">html</a> </li>
-        <li> <a href="/read/2">html</a> </li>
-        <li> <a href="/read/3">html</a> </li>
+        {lis}
       </ol>
     </nav>
   )
 }
 
-function Acticel() {
+function Acticel(props) {
   return(
     <acticle>
-      <h2>Welcom</h2>
-      Hello, Web
+      <h2>{props.title}</h2>
+      {props.body}
     </acticle>
   )
 }
 
 function App() {
+  let topics = [
+    {id : 1, title : "html", body : "html is ..."},
+    {id : 2, title : "css", body : "css is ..."},
+    {id : 3, title : "js", body : "js is ..."}
+  ]
   return (
     <div>
-      <Header ></Header>
-      <MenuList></MenuList>
-      <Acticel></Acticel>
+      <Header title="React" ></Header>
+      <Nav topics={topics} ></Nav>
+      <Acticel title="Welcome" body="Hellow, Web" ></Acticel>
     </div>
   );
 }
